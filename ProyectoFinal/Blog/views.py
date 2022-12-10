15bfcc7efbfd822c7e76f7694ff320ce1ee2a642
guidomcanle <guidocanle@gmail.com>
2022-12-10 11:11:25 -0300

    Dandole forma a la app Blog

--- ProyectoFinal/Blog/views.py
+++ ProyectoFinal/Blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
+#from .forms import *
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def inicio(request):
+
+    return render(request, 'Blog/static/Blog/index.html')
--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ProyectoFinal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
